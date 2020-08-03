895b837f853867313addf00c32ccb5bb543de1db
badesz97 <badesz97@gmail.com>
2020-08-03 17:41:13 +0200

    First commit

---
+++ src/git_test/Program.java
@@ -0,0 +1,16 @@
+package git_test;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		
+		if (hungry.equals("yes")) {
+			System.out.println("Eat something!");
+		} else {
+			System.out.println("Do some workout!");
+		}
+	}
+
+}
