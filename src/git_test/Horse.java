038100583466a313a175fba5e78f5d3353fcda25
badesz97 <badesz97@gmail.com>
2020-08-05 11:45:50 +0200

    Horse class added

---
+++ src/git_test/Horse.java
@@ -0,0 +1,32 @@
+package git_test;
+
+public class Horse {
+
+	String name;
+	int age;
+	int horseSpeed;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getHorseSpeed() {
+		return horseSpeed;
+	}
+	public void setHorseSpeed(int horseSpeed) {
+		this.horseSpeed = horseSpeed;
+	}
+	public Horse(String name, int age, int horseSpeed) {
+		this.name = name;
+		this.age = age;
+		this.horseSpeed = horseSpeed;
+	}
+}
