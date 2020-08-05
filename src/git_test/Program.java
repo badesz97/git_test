3f983f785e88191c9c3659fe977542c42417670b
szalai_geri <szerg97@gmail.com>
2020-08-05 14:20:14 +0200

    isHungry added and printed

--- src/git_test/Program.java
+++ src/git_test/Program.java
@@ -11,11 +11,12 @@ public class Program {
 	public static void main(String[] args) {
 
 		String hungry = "yes";
-		
+		boolean isHungry = false;
 		if (hungry.equals("yes")) {
 			System.out.println("Eat something now!");
 		} else {
 			System.out.println("Do some workout right now bitch!");
+			System.out.println(isHungry);
 		}
 	}
 }
