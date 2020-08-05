cb63449f83fb5d525d6e32fb9020939674494fc3
szalai_geri <szerg97@gmail.com>
2020-08-05 10:52:49 +0200

    Cat class created

---
+++ src/git_test/Cat.java
@@ -0,0 +1,28 @@
+package git_test;
+
+public class Cat {
+
+	private String name;
+	private String speed;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpeed() {
+		return speed;
+	}
+	public void setSpeed(String speed) {
+		this.speed = speed;
+	}
+	public Cat(String name, String speed) {
+		super();
+		this.name = name;
+		this.speed = speed;
+	}
+	
+	
+	
+}
