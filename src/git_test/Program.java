45218990e6f770628916149c990e79e797b562d1
szalai_geri <szerg97@gmail.com>
2020-08-12 15:03:53 +0200

    test commit

--- src/git_test/Program.java
+++ src/git_test/Program.java
@@ -9,48 +9,39 @@ import javax.swing.JFrame;
 
 import git_test.entity.*;
 
-
 public class Program {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Company comp1 = new Company(1, "ForumDigital", "This is my company", new Document(1,"Szerzodesek", "Ez egy leiras."));
-=======
+		Company comp1 = new Company(1, "ForumDigital", "This is my company",
+				new Document(1, "Szerzodesek", "Ez egy leiras."));
 
-		Company comp1 = new Company(1, "ForumDigital", "This is my company", new Document(1,"Szerzodesek", "Ez egy leiras."));
-		System.out.println(comp1.toString());
-		
->>>>>>> branch 'geri' of https://github.com/badesz97/git_test.git
-		
 		try {
 
-			for (int i = 0; i < 3; i++) {
-				String filename = File.separator + "tmp";
-				JFileChooser fc = new JFileChooser(new File(filename));
-				JFrame frame = new JFrame();
+			String filename = File.separator + "tmp";
+			JFileChooser fc = new JFileChooser(new File(filename));
+			JFrame frame = new JFrame();
 
-				// Show open dialog; this method does not return until the dialog is closed
-				fc.showOpenDialog(frame);
-				File selFile = fc.getSelectedFile();
+			// Show open dialog; this method does not return until the dialog is closed
+			fc.showOpenDialog(frame);
+			File selFile = fc.getSelectedFile();
 
-				// Show save dialog; this method does not return until the dialog is closed
-				// fc.showSaveDialog(frame);
-				// selFile = fc.getSelectedFile();
+			// Show save dialog; this method does not return until the dialog is closed
+			// fc.showSaveDialog(frame);
+			// selFile = fc.getSelectedFile();
 
-				printContent(selFile);
-				
-				comp1.getDocument().getFiles().add(selFile);
-			}
+			printContent(selFile);
+
+			comp1.getDocument().getFiles().add(selFile);
 
 			System.out.println(comp1.getDocument().getFiles().size());
 		} catch (Exception e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-		
-		//Adding file to companies documents
+
+		// Adding file to companies documents
 	}
-	
+
 	private static void printContent(File file) {
 		try {
 			Scanner myReader = new Scanner(file);
@@ -59,10 +50,6 @@ public class Program {
 				System.out.println(data);
 			}
 			myReader.close();
-			
-			comp1.getDocument().getFiles().add(selFile);
-			
-			System.out.println(comp1.getDocument().getFiles().size());
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
