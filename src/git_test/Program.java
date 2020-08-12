01bbaf027bd20dfad3ccd7d032ef7e058534b446
badesz97 <badesz97@gmail.com>
2020-08-12 11:18:53 +0200

    file browser added

--- src/git_test/Program.java
+++ src/git_test/Program.java
@@ -1,5 +1,14 @@
 package git_test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import git_test.entity.Company;
+
 /*
  * 
  * Change BC
@@ -8,6 +17,35 @@ package git_test;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
+		try {
+
+			Company comp1 = new Company(1, "ForumDigital", "This is my company");
+
+			System.out.println("Press enter for chosing a file.");
+
+			String filename = File.separator + "tmp";
+			JFileChooser fc = new JFileChooser(new File(filename));
+
+			JFrame frame = new JFrame();
+
+			// Show open dialog; this method does not return until the dialog is closed
+			fc.showOpenDialog(frame);
+			File selFile = fc.getSelectedFile();
+
+			// Show save dialog; this method does not return until the dialog is closed
+			// fc.showSaveDialog(frame);
+			// selFile = fc.getSelectedFile();
+
+			Scanner myReader = new Scanner(selFile);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/git_test/entity/Company.java
@@ -0,0 +1,55 @@
+package git_test.entity;
+
+
+public class Company {
+	
+	private int id;
+	private String name;
+	private String description;
+	private Document document;
+	
+	public Company(int id, String name, String description) {
+		
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
+
+	@Override
+	public String toString() {
+		return "Company [id=" + this.id + ", name=" + this.name + ", description=" + this.description +  ", document=" + this.document + "]";
+	}
+
+}
